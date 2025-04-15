a88144889464de24c0ee8a28fb23621a87943c7a
Imoite-zn <lawrenceimoite@gmail.com>
2025-04-15 10:35:43 -0700

    add themes

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,3 +1,9 @@
+import 'package:compl_ecom_apparelz/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:compl_ecom_apparelz/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:compl_ecom_apparelz/utils/theme/custom_themes/check_box_theme.dart';
+import 'package:compl_ecom_apparelz/utils/theme/custom_themes/chip_theme.dart';
+import 'package:compl_ecom_apparelz/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:compl_ecom_apparelz/utils/theme/custom_themes/text_form_field_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:compl_ecom_apparelz/utils/theme/custom_themes/text_theme.dart';
 
@@ -13,7 +19,13 @@ class ZAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     textTheme: ZTextTheme.lightTextTheme,
-    elevatedButtonTheme: ZElevatedButtonTheme.darkELevatedButtonTheme,
+    elevatedButtonTheme: ZElevatedButtonTheme.lightElevatedButtonTheme,
+    appBarTheme: ZAppBarTheme.lightAppBarTheme,
+    chipTheme: ZChipTheme.lightChipTheme,
+    inputDecorationTheme: ZTextFormFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: ZOutlinedButtonTheme.lightOutlinedButtonTheme,
+    bottomSheetTheme: ZBottomSheetTheme.lightBottomSheetTheme,
+    checkboxTheme: ZCheckBoxTheme.lightCheckBoxTheme,
   );
 
 
@@ -24,6 +36,13 @@ class ZAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: ZTextTheme.darkTextTheme,
+    elevatedButtonTheme: ZElevatedButtonTheme.darkELevatedButtonTheme,
+    appBarTheme: ZAppBarTheme.darkAppBarTheme,
+    chipTheme: ZChipTheme.darkChipTheme,
+    inputDecorationTheme: ZTextFormFieldTheme.darkInputDecorationTheme,
+    outlinedButtonTheme: ZOutlinedButtonTheme.darkOutlinedButtonTheme,
+    bottomSheetTheme: ZBottomSheetTheme.darkBottomSheetTheme,
+    checkboxTheme: ZCheckBoxTheme.darkCheckBoxTheme,
   );
 
   
